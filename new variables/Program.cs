38657db08b37a19264642d1c07ecc937e70b7067
Bibhot1 <bndazbvu.me12@gmail.com>
2024-05-17 07:05:26 +1200

    Add project files.

---
+++ new variables/Program.cs
@@ -0,0 +1,11 @@
+﻿// String manipulation and formatting
+
+int num = 10;
+double price = 20.23;
+string name = "Bibhot";
+//Interpolation
+Console.WriteLine($"The number is {num}");
+//Concationation
+Console.WriteLine("The number is "+ num);
+//String formatting
+Console.WriteLine("The number is {1}, and the price is {0}, and the owner is {2}", num,price,name);
